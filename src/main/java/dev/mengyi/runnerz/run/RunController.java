5593532472963f6eba89c272c8de793c88b1c79c
mengyi <yiii9292@gmail.com>
2024-08-01 18:14:59 -0400

    update, delete, create Run exception, POSTMAN and IntelliJ api test

--- src/main/java/dev/mengyi/runnerz/run/RunController.java
+++ src/main/java/dev/mengyi/runnerz/run/RunController.java
@@ -31,8 +31,26 @@ public class RunController {
     Run findById(@PathVariable Integer id) {
         Optional<Run> run = runRepository.findById(id);
         if(run.isEmpty()){
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Run not found");
+            throw new RunNotFoundException(id);
         }
         return run.get();
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    void create(@RequestBody Run run) {
+        runRepository.create(run);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    void update(@RequestBody Run run, @PathVariable Integer id) {
+        runRepository.update(run, id);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    void delete(@PathVariable Integer id) {
+        runRepository.delete(id);
     }
 }
--- src/main/java/dev/mengyi/runnerz/run/RunRepository.java
+++ src/main/java/dev/mengyi/runnerz/run/RunRepository.java
@@ -4,7 +4,6 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -19,14 +18,29 @@ public class RunRepository {
 
     @PostConstruct
     private void init(){
-        runs.add(new Run(1, "First Run", LocalDateTime.now(), LocalDateTime.now().plus(30, ChronoUnit.MINUTES), 5, Location.OUTDOOR));
-        runs.add(new Run(2, "Second Run", LocalDateTime.now(), LocalDateTime.now().plus(60, ChronoUnit.MINUTES), 6, Location.OUTDOOR));
-        runs.add(new Run(3, "Third Run", LocalDateTime.now(), LocalDateTime.now().plus(90, ChronoUnit.MINUTES), 10, Location.OUTDOOR));
+        runs.add(new Run(1, "First Run", LocalDateTime.now(), LocalDateTime.now().plusMinutes(30), 5, Location.OUTDOOR));
+        runs.add(new Run(2, "Second Run", LocalDateTime.now(), LocalDateTime.now().plusMinutes(60), 6, Location.OUTDOOR));
+        runs.add(new Run(3, "Third Run", LocalDateTime.now(), LocalDateTime.now().plusMinutes(90), 10, Location.OUTDOOR));
     }
 
     Optional<Run> findById(Integer id) {
 //                return runs.stream().filter(run -> run.id() == id).findFirst().get();
 //        if the Optional is empty, no Run object matches the filter, so it will throw a NoSuchElementException
         return runs.stream().filter(run -> run.id() == id).findFirst();
+    }
+
+    void create(Run run) {
+        runs.add(run);
+    }
+
+    void update(Run run, Integer id) {
+        Optional<Run> runToUpdate = findById(run.id());
+        if(runToUpdate.isPresent()){
+            runs.set(runs.indexOf(runToUpdate.get()), run);
+        }
+    }
+
+    void delete(Integer id) {
+        runs.removeIf(run -> run.id() == id);
     }
 }
